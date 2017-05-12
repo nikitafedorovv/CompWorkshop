68ce07cb5846adc09756c488fee5dae50da4d385
Nikita Fedorov <nikitafedorovv@gmail.com>
2017-05-13 02:20:34 +0300

    Task 7 collocation method implemented

--- src/nf/Task7.java
+++ src/nf/Task7.java
@@ -1,259 +1,102 @@
 package nf;
 
-import java.util.Scanner;
+import Jama.Matrix;
 
 public class Task7 {
+    private static double alpha = 3.6;
+    private static int n = 10; //для метода коллокации
 
-    private static void swap(double[][] A, int i, int j, int k, int p){
-        double c = A[i][j];
-        A[i][j] = A[k][p];
-        A[k][p] = c;
+    //коэффициенты граничных условий
+    private static double a1 = 1;
+    private static double a2 = 0;
+    private static double b1 = 1;
+    private static double b2 = 0;
+
+    private static double p(double x){
+        return 1;
+    }
+    private static double q(double x){
+        return -(x * x + alpha);
+    }
+    private static double r(double x){
+        return -2 * x;
+    }
+    private static double f(double x){
+        return 2 * (3 * x * x - alpha) / Math.pow((x * x + alpha), 3) + 2 * x / (alpha + 1);
+    }
+    private static double solution(double x){
+        return 1 / (x * x + alpha) - 1 / (alpha + 1);
     }
 
-    //решение
-    private static double F(double x, int alpha) {
-        return 1.0/(x*x+alpha)-1.0/(alpha+1);
+    private static double chebyshev(int i){ //i=1..n
+        return Math.cos(Math.PI * (2 * i - 1) / (double)(2 * n));
     }
-    //полином Якоби считаем
-    private static double P(double x, int i, int k) {
-        if (k==0) {
-            if (i==0)
-                return 1;
-            if (i == 1)
-                return 2*x;
-            else
-                return ((i+1)*(2*i+1)*P(x,i-1,k)*x-(i+1)*i*P(x,i-2,k))/(double)(i+2)/(double)(i);
+
+    private static double P(double x, int i, int k){ //Л i=0..
+        if (k == 0){
+            if (i == 0) return 1;
+            if (i == 1) return 2 * x;
+            if (i > 1) return ((i+1)*(2*i+1)*P(x,i-1,k)*x-(i+1)*i*P(x,i-2,k))/((i+2)*i);
+        }
+        if (k == 1){
+            if (i == 0) return 0;
+            if (i == 1) return 2;
+            if (i > 1) return ((i+1)*(2*i+1)*(P(x,i-1,0)+x*P(x,i-1,1))-(i+1)*i*P(x,i-2,1))/((i+2)*i);
+        }
+        if (k == 2){
+            if ((i == 0) || (i == 1)) return 0;
+            if (i > 1) return ((i+1)*(2*i+1)*(2*P(x,i-1,1)+x*P(x,i-1,2))-(i+1)*i*P(x,i-2,2))/((i+2)*i);
         }
 
-        if (k==1) {
-            if (i==0)
-                return 0;
-            if (i==1)
-                return 2;
-            else
-                return ((i+1)*(2*i+1)*(P(x,i-1,0)+x*P(x,i-1,1))-(i+1)*i*P(x,i-2,1))/(i+2)/i;
-        }
-
-        else { //k=0/1/2
-            if (i==0)
-                return 0;
-            if (i==1)
-                return 0;
-            else
-                return ((i+1)*(2*i+1)*(2*P(x,i-1,1)+x*P(x,i-1,2))-(i+1)*i*P(x,i-2,2))/(i+2)/i;
-        }
+        System.out.println("Чёт с индексами i/k намутил в P(double x, int i, int k) :(" + i + " " + k);
+        return 0;
     }
 
-    //сетка
-    private static double w( double x, int i, int k) { //функциональная сетка, k-порядок производной
-        if (k==0)
-            return (1-x*x)*P(x,i,k);
-        if (k==1)
-            return -2*x*P(x,i,0)+(1-x*x)*P(x,i,1);
-        else
-            return -2*P(x,i,0)-2*x*P(x,i,1)-2*x*P(x,i,1)+(1-x*x)*P(x,i,2);
+    private static double w(double x, int i, int k){ //НП i=1..n
+        //i++;
+        if (k == 0) return (1 - x * x) * P(x, i - 1, 0);
+        if (k == 1) return -2 * x * P(x, i - 1, 0) + (1 - x * x) * P(x, i - 1, 1);
+        if (k == 2) return -2 * P(x, i - 1, 0) - 4 * x * P(x, i - 1, 1) + (1 - x * x) * P(x, i - 1, 2);
+
+        System.out.println("Чёт с производной намутил в w(double x, int i, int k) :(");
+        return 0;
     }
 
-    //Для решения системы линейных уравнений
-    private static int find_max_stol(double[][] A,int n,int k)
-    {
-        int max_st=k;
-        double max=Math.abs(A[k][k]);
-        for (int i=k+1;i<n;i++)
-            if (max<Math.abs(A[i][k]))
-            {
-                max_st=i;
-                max=Math.abs(A[i][k]);
+    private static double LWi(double x, int i){//i=1..n
+        return p(x) * w(x, i, 2) + q(x) * w(x, i, 1) + r(x) * w(x, i, 0);
+    }
+
+    private static double collocSolution(double x, Matrix c){
+        double sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += c.get(i, 0) * w(x, i + 1, 0);
+        }
+        return sum;
+    }
+
+    private static void collocation(){
+        Matrix system = new Matrix(n, n);
+        Matrix b = new Matrix(n, 1);
+        for(int j = 0; j < n; j++){
+            b.set(j, 0, f(chebyshev(j + 1)));
+            for(int i = 0; i < n; i ++){
+                system.set(j, i, LWi(chebyshev(j + 1),i + 1));
             }
-        //cout <<max_st<<endl;
-        return max_st;
-    }
-
-    private static void main_stol(double A[][], int n, double x[])
-    {
-        for (int k=0;k<n;k++)
-        {
-            int max_st=find_max_stol(A,n,k);
-            for (int j=0;j<=n;j++)
-                swap(A,k,j,max_st,j);
-
-            if (A[k][k]!=0)
-            {
-                double mult = A[k][k];
-                for (int j=k;j<=n;j++)
-                    A[k][j]=A[k][j]/mult;
-            }
-
-            for (int i=k+1;i<n;i++)
-            {
-                double mult = A[i][k];
-                for (int j=k;j<=n;j++)
-                    A[i][j]=A[i][j]-A[k][j]*mult;
-            }
-
-		/*for (int i=0;i<n;i++)
-		{
-		for (int j=0;j<=n;j++)
-		cout<< A[i][j]<<' ';
-		cout <<endl<<endl;
-		}*/
         }
+        Matrix c = system.solve(b);
 
-        for (int i=n-1;i>=0;i--)
-        {
-            double tmp=0;
-            for (int j=i+1;j<n;j++)
-                tmp+=A[i][j]*x[j];
-            x[i]=A[i][n]-tmp;
-        }
+        System.out.println("-0.5: " + solution(-0.5) + " " + collocSolution(-0.5, c) + " " + Math.abs(solution(-0.5) - collocSolution(-0.5, c)));
+        System.out.println("   0: " + solution(0) + " " + collocSolution(0, c) + " " + Math.abs(solution(0) - collocSolution(0, c)));
+        System.out.println(" 0.5: " + solution(0.5) + " " + collocSolution(0.5, c) + " " + Math.abs(solution(0.5) - collocSolution(0.5, c)));
+
     }
 
+    private static void galerkin(){
 
-    private static double L(int i, int j, double x[],double p[], double q[],double r[])
-    {
-        return p[i]*w(x[i],j,2)+q[i]*w(x[i],j,1)+r[i]*w(x[i],j,0);
     }
 
-
-    //наше уравнение, которое нам дано
-    private static double LL(int i, double x,int alpha)
-    {
-        return 1*w(x,i,2)-(x*x+alpha)*w(x,i,1)-2*x*w(x,i,0);
+    public static void go(){
+        collocation();
+        galerkin();
     }
-
-    //вычисление интегралов для метода Галеркина
-    private static double MyFunction(double x, int i, int j,int alpha, int who)
-    {
-        if (who==1)
-            return w(x,j,0)*LL(i,x,alpha);
-        else
-            return (2*(3*x*x-alpha)/(x*x+alpha)/(x*x+alpha)/(x*x+alpha)+2*x/(alpha+1))*w(x,j,0);
-    }
-
-    //метод Симпсона для решения интеграла
-    private static double SimpsonIntegration(int n, int i,int j, int alpha,int who) //для поиска коэффициентов матриц
-    {
-        int a=-1;
-        int b=1;
-        n=100;
-        double h = ((double)b - (double)a) / (double)n/2;
-        double IntegSum = MyFunction(a,i,j,alpha,who)+MyFunction(b,i,j,alpha,who);
-        for (int k = 2; k<=2*n-2; k+=2 )
-            IntegSum+=2*MyFunction(a+k*h,i,j,alpha,who);
-        for (int k = 1; k<=2*n-1; k+=2 )
-            IntegSum+=4*MyFunction(a+k*h,i,j,alpha,who);
-        IntegSum*=h/3;
-        return IntegSum;
-    }
-
-
-    private static void Galyorkin_method(int n, int alpha,double y[]) {
-        double A[][]=new double[n][];
-        for (int i=0;i<n;i++)
-            A[i]=new double[n+1];
-
-        for (int j=0;j<n;j++)
-            for (int i=0;i<n;i++)
-                A[j][i]=SimpsonIntegration(n,i,j,alpha,1);
-
-        for (int i=0;i<n;i++)
-            A[i][n]=SimpsonIntegration(n,i,i,alpha,2);
-
-        double c[] = new double[n];
-        main_stol(A,n,c);
-
-        double result[]=new double[3];
-        System.out.println("Метод Галеркина: ");
-        for (int i=0;i<3;i++)
-        {
-            double sum=0;
-            for (int j=0;j<n;j++)
-                sum+=c[j]*w(-0.5+0.5*i,j,0);
-            System.out.println("y[" + (-0.5+0.5*i) + "] = " + sum);
-            result[i]=y[i]-sum;
-        }
-
-        System.out.println("Погрешность:");
-        for (int i=0;i<3;i++)
-            System.out.println(result[i] + " ");
-    }
-
-    public static void go() {
-        System.out.println("Методы решения ОДУ в аналитическом виде");
-        System.out.println("Введите n");
-        int n;
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        System.out.println("[a, b] = ["+(-1)+", "+1+"]");
-
-        int a=-1;
-        int b=1;
-        int alpha=4;
-
-        double  x[] = new double[n];
-        double pi=3.141592653589793;
-        // густая сетка узлов (корни многочлена Чебышева 1 порядка)
-        for (int i=0;i<n;i++)
-            x[i]=Math.cos((2*i+1)*pi/2.0/(double)(n));
-
-        double p[]=new double[n];
-        for (int i=0;i<n;i++) p[i]=1;
-
-        double f[]=new double[n];
-        for (int i=0;i<n;i++)
-            f[i]=2*(3*x[i]*x[i]-alpha)/(x[i]*x[i]+alpha)/(x[i]*x[i]+alpha)/(x[i]*x[i]+alpha)+2*x[i]/(alpha+1);
-
-        double r[]=new double[n];
-        for (int i=0;i<n;i++)
-            r[i]=-2*x[i];
-
-        double q[]=new double[n];
-        for (int i=0;i<n;i++)
-            q[i]=-(x[i]*x[i]+alpha);
-
-        double A[][]=new double[n][];
-        for (int i=0;i<n;i++)
-            A[i]=new double[n+1];
-
-        //заполняем матрицу А и f, и решаем методом главного элемента по столбцу (таким образом решение - с(i)
-	    double c[]=new double[n];
-        for (int i=0;i<n;i++)
-            for (int j=0;j<n;j++)
-                A[i][j]=L(i,j,x,p,q,r);
-
-        for (int i=0;i<n;i++)
-            A[i][n]=f[i];
-
-        main_stol(A,n,c);
-
-
-	    double y[]=new double[3];
-
-        System.out.println("Точное решение:");
-        for (int i=0;i<3;i++)
-        {
-            y[i]=F(-0.5+0.5*i,alpha);
-            System.out.println("y[" + (-0.5+0.5*i) + "] = "+y[i]);
-        }
-
-
-        System.out.println("Метод коллокаций: ");
-	    double result[]=new double[3];
-        for (int i=0;i<3;i++)
-        {
-            double sum=0;
-            for (int j=0;j<n;j++)
-                sum+=c[j]*w(-0.5+0.5*i,j,0);
-            System.out.println("y[" + (-0.5+0.5*i) + "] = "+sum);
-            result[i]=y[i]-sum;
-        }
-
-        System.out.println("Погрешность:");
-        for (int i=0;i<3;i++)
-            System.out.println(result[i]+" ");
-
-        //метод Галеркина
-        Galyorkin_method(n,alpha,y);
-    }
-
 }
